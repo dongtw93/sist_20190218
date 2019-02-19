20cdeeb6b0d2173be04a210d04875a45cf4a01fd
min0425 <min0425@naver.com>
2019-02-19 09:53:04 +0900

    20190219 수업 내용

---
+++ 01.JavaSE/javase_20190219/src/com/test/Program009.java
@@ -0,0 +1,40 @@
+/*
+ * 프로그램명: 자료형 - 기본 자료형
+ * 작성자 : 이민종
+ * 작성일 : 20190219
+*/
+
+package com.test;
+
+public class Program009 {
+
+	public static void main(String[] args) {
+		
+		//자료형 구분
+		//1. 기본 자료형(Primitive Data Types)
+		//2. 참조 자료형(Reference/Object Data Types)
+		
+
+		//1. 기본 자료형(Primitive Data Types)
+		//정수, 실수, 문자, 부울(논리값)
+		//정수- byte, short, int, long
+		//실수- float, double
+		//문자- char
+		//부울- boolean
+		
+		//변수 a라는 공간에 100 이라는 값 저장
+		//변수 a는 byte 자료형만큼의 공간을 차지한다
+		byte a = 100;
+		
+		//변수 b라는 공간에 200 이라는 값 저장
+		//변수 b는 short 자료형만큼의 공간을 차지한다
+		short b = 200;
+		
+		
+		
+		
+		
+		
+	}
+
+}
