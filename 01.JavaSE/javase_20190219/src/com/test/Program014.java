00c14f75c0af97487a15001c81f09ae95e04d2ba
min0425 <min0425@naver.com>
2019-02-19 11:38:37 +0900

    20190219

--- 01.JavaSE/javase_20190219/src/com/test/Program014.java
+++ 01.JavaSE/javase_20190219/src/com/test/Program014.java
@@ -9,26 +9,16 @@ package com.test;
 public class Program014 {
 
 	public static void main(String[] args) {
+
+		//문제] 코드 분석
 		
-		//----------------------
-		//입력 단계
-		//시험 과목들
-		//시험 점수를 저장할 변수들
-		//시험 성적(과목1, 과목2, 과목3) 입력
-		
-		//동일 자료형의 변수들은 같이 선언 가능
-		//변수 선언과 초기값 할당을 같이 선언 가능
-		//int 자료형은 기본 초기값 0
-		//double 자료형의 기본 초기값 0.0
-		int kor = 60, eng = 70, math = 80;
-		int total = 0;
-		double avg = 0.0;
-				
-		//----------------------
-		//출력 단계
-		//시험 성적(총점, 평균) 출력
-		System.out.printf("총점:%d, 평균:%f", total, avg);
-		
+		//
+		int age = 0;
+		//
+		age = age + 7;
+		//
+		System.out.println("Puppy age is : " + age);
+
 	}
 
 }
