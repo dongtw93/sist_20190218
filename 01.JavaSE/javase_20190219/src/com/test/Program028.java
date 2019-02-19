43157deadc2b92f54a370efbb2b1f60d1efb367d
min0425 <min0425@naver.com>
2019-02-19 17:26:02 +0900

    문제) 성적 처리 결과 출력

--- 01.JavaSE/javase_20190219/src/com/test/Program028.java
+++ 01.JavaSE/javase_20190219/src/com/test/Program028.java
@@ -26,8 +26,25 @@ public class Program028 {
 		총점: 268
 		평균: 89.3
 		*/
-
 		
+		//입력 과정
+		int kor = 100, eng = 90, math = 78;
+		int sum = 0;
+		double avg = 0.0;
+		
+		//처리 과정
+		sum = 100 + 90 + 78;
+		avg = sum / 3.0;
+		
+		//출력 과정
+		System.out.printf("-----------------%n");
+		System.out.printf("    성적출력      %n");
+		System.out.printf("-----------------%n");
+		System.out.printf("과목1: %d%n", kor);
+		System.out.printf("과목2: %d%n", eng);
+		System.out.printf("과목3: %d%n", math);
+		System.out.printf("총점: %d%n", sum);
+		System.out.printf("평균: %.1f%n", avg);
 		
 	}
 
