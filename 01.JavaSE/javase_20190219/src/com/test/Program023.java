eea1aedb8219582bb9b97a5fdcbf6723531acdbe
min0425 <min0425@naver.com>
2019-02-19 15:36:41 +0900

    문제) 실수 연산

---
+++ 01.JavaSE/javase_20190219/src/com/test/Program023.java
@@ -0,0 +1,35 @@
+/*
+ * 프로그램명: 연산자
+ * 작성자 : 이민종
+ * 작성일 : 20190219
+*/
+
+package com.test;
+
+public class Program023 {
+
+	public static void main(String[] args) {
+		
+		//문제) 실수 연산
+		//실수 연산시 정밀도 제한 때문에 소수 이하 결과가 올바르지 못할 수 있다.
+		//->정수화 시켜서 연산 필요
+		
+		//입력 과정
+		//변수 선언 및 초기값 할당
+		double a = 0.1, b, c, d;
+		
+		//처리 과정
+		b = a * 5;
+		c = a * 6;
+		d = a * 7;
+		
+		//출력 과정
+		System.out.println(a); //??
+		System.out.println(b); //??
+		System.out.println(c); //??
+		System.out.println(d); //??
+		
+		
+	}
+
+}
