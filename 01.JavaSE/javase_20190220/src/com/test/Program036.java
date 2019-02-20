85bdde14665bef106e2d0722e57bf4f0be2e7bb3
min0425 <min0425@naver.com>
2019-02-20 10:51:59 +0900

    제어구조-if~else if~else문

---
+++ 01.JavaSE/javase_20190220/src/com/test/Program036.java
@@ -0,0 +1,34 @@
+/* 
+ * 프로그램명: 제어구조-if~else if~else문
+ * 작성자 : 이민종
+ * 작성일 : 20190220
+ * 
+ */
+
+package com.test;
+
+public class Program036 {
+
+	public static void main(String[] args) {
+		
+		//첫 번째 조건식의 결과가 true인 경우 if블럭({}) 안에 있는 실행문 실행 가능
+		//첫 번째 조건식의 결과가 false인 경우 else if블럭({})의 조건식 검사
+		//else if 블럭의 조건식 결과가 true인지, false인지에 따라서 특정 실행문 실행 가능
+		//조건식 - 비교, 논리 연산자를 이용한 수식
+		/*
+		if(Boolean_expression 1) {
+		   // Executes when the Boolean expression 1 is true
+		} else if(Boolean_expression 2) {
+		   // Executes when the Boolean expression 2 is true
+		} else if(Boolean_expression 3) {
+		   // Executes when the Boolean expression 3 is true
+		} else {
+		   // Executes when the none of the above condition is true.
+		} 
+		*/
+		
+		
+		
+	}
+
+}
