fa9da6a3f36efc6e967d8f57e869864c875e989f
min0425 <min0425@naver.com>
2019-02-21 11:50:00 +0900

    배열(Array)

---
+++ 01.JavaSE/javase_20190221/src/com/test/Program066.java
@@ -0,0 +1,41 @@
+/* 
+ * 프로그램명: 배열
+ * 작성자 : 이민종
+ * 작성일 : 20190221
+ *  
+ */
+
+package com.test;
+
+public class Program066 {
+
+	public static void main(String[] args) {
+		
+		//배열(Array)
+		//여러개의 자료를 하나의 저장소에 저장할 수 있는 공간
+		//일반적인 저장소인 변수 공간에는 자료 한 개만 저장 가능
+		//예를 들어, int a = 10; a라는 공간에 숫자 10만 저장
+		//예를 들어, int[] a = {10, 20}; a라는 공간에 숫자 10, 20를 같이 저장 가능
+		
+		//배열은 참조형 자료형이다. 일반적인 저장소인 기본형 자료형과 다르게 운영된다.
+		//참조형 자료형은 변수 선언 공간과 자료 저장 공간이 분리되어 있다. 참조주소에 의해 연결되어 있다.
+		//예를 들어, 마트내의 진열대의 물건을 그대로 가져올 수 있으면 기본형.
+		//예를 들어, 마트내의 진열대의 샘플만 존재. 실제 구매를 원하는 물건은 별도의 장소에 저장되어 있다. 참조형.	
+
+		/*
+		
+		방법1)
+		dataType[] arrayRefVar; 
+		arrayRefVar = new dataType[arraySize];
+		arrayRefVar[idx] = data;
+		arrayRefVar[idx]
+		
+		방법2)
+		dataType[] arrayRefVar = {data1, data2, ...};
+		arrayRefVar[idx]
+		
+		*/
+		
+	}
+
+}
