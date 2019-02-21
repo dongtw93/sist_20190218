42675dac5c35804e56fb2b510eb9dd85ed35e19e
min0425 <min0425@naver.com>
2019-02-21 16:35:56 +0900

    거품 정렬(Bubble Sort)

--- 01.JavaSE/javase_20190221/src/com/test/Program070.java
+++ 01.JavaSE/javase_20190221/src/com/test/Program070.java
@@ -18,16 +18,35 @@ public class Program070 {
 		//-> 석차 알고리즘 구현
 
 		//배열 준비
-		int arr[] ={3,60,35,2,45,320,5};  
+		int arr[] ={60, 3, 35, 2, 45, 320, 5};  
+		
+		//원본 상태 출력
+		System.out.println(java.util.Arrays.toString(arr));
 		
 		//거품 정렬(Bubble Sort) 
 		//->인접한 두 요소 비교 후, 큰 숫자를 오른쪽으로 이동.
 		//->1회전시 가장 큰 숫자가 가장 오른쪽에 저장.
 		//->n회전시 모든 숫자가 정렬된다. n은 요소 전체 갯수 - 1
-		
-		
-		
 
+		/*
+		if (arr[0] > arr[1]) {
+			int temp = arr[1];
+			arr[1] = arr[0];
+			arr[0] = temp;
+		}
+		*/
+		
+		for (int b = 0; b < arr.length - 1; ++b) {
+			if (arr[b] > arr[b + 1]) {
+				int temp = arr[b + 1];
+				arr[b + 1] = arr[b];
+				arr[b] = temp;
+			}
+		}
+		
+		//1회전 상태 출력
+		System.out.println(java.util.Arrays.toString(arr));
+		
 	}
 
 }
