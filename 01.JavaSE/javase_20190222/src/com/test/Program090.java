33c1eb63d9c9ea9953288ca620033c9cdf6124a3
min0425 <min0425@naver.com>
2019-02-22 15:14:54 +0900

    1차원 배열 -> 2차원 배열 변환

--- 01.JavaSE/javase_20190222/src/com/test/Program090.java
+++ 01.JavaSE/javase_20190222/src/com/test/Program090.java
@@ -14,11 +14,20 @@ public class Program090 {
 		//1차원 배열 -> 2차원 배열 변환
 		//랜덤한 크기(~30)의 1차원 배열 요소(1부터 순차적으로)를 
 		//2차원 배열로 변환
-		   
+		//출력 예)
+		/*
+		[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+		 1  2  3  4  5 
+		 6  7  8  9 10 
+		11 12 13 14 15 
+		16 17 18 19 20 
+		21 22 23 24 25 
+		*/
+		
 		
 		//1차원 배열로 변환
 		//->배열은 선언시 크기 지정
-		int[] arr = new int[25];
+		int[] arr = new int[23];
 		int len = arr.length;
 		
 		for (int a = 0; a < len; ++a) {
@@ -34,12 +43,23 @@ public class Program090 {
 		
 		//2차원 빈 배열 준비
 		int count = 5;
+		
+		//몫 연산
 		int row = arr.length / count;
+		//주의) 나머지가 존재하는 경우 별도 처리 필요
+		int mod = arr.length % count;
+		if (mod > 0) {
+			++row;
+		}
+				
 		int[][] arr2 = new int[row][];
 		
 		//각 행별로 1차원 빈 배열 준비
 		int c = 0;
 		for (int a = 0; a < row; ++a) {
+			if (mod > 0 && a == row - 1) {
+				count = mod;
+			} 
 			arr2[a] = new int[count];
 			//1차원 배열의 요소를 2차원 배열로 복사
 			for (int b = 0; b < count; ++b) {
