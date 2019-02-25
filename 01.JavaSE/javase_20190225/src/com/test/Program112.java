606b8d8d6372538db48271c22670fbf60fc170b7
min0425 <min0425@naver.com>
2019-02-25 16:34:34 +0900

    회원정보관리-회원정보입력+회원정보출력

--- 01.JavaSE/javase_20190225/src/com/test/Program112.java
+++ 01.JavaSE/javase_20190225/src/com/test/Program112.java
@@ -49,6 +49,8 @@ public class Program112 {
 		//주의) 2차원 배열 선언시 1차원 저장소의 크기 지정 생략
 		String[][] members = new String[10][];
 		
+		//2차원 배열의 인덱싱을 위한 임시변수
+		int cnt = 0;
 		
 		while (true) {
 			System.out.println();
@@ -65,10 +67,13 @@ public class Program112 {
 				System.out.println("------------------------");
 				System.out.println("이름 / 전화번호 / 이메일");
 				for (int a = 0; a < len; ++a) {
+					//2차원 배열의 요소를 순차적 접근
+					String[] member = members[a];
+					//1차원 배열의 요소를 인덱스 지정 접근
 					System.out.printf("%s / %s / %s%n"
-							, members[a][0]
-							, members[a][1]
-							, members[a][2]);
+							, member[0]
+							, member[1]
+							, member[2]);
 				}
 				
 			} else if (m == 2){
@@ -77,10 +82,35 @@ public class Program112 {
 				
 				//문제) 회원정보입력 코드 구현
 				
+				//이름, 전화번호, 이메일 외부 입력
+				System.out.print("이름>");
+				String name = sc.nextLine();
+				System.out.print("전화번호>");
+				String phone = sc.nextLine();
+				System.out.print("이메일>");
+				String email = sc.nextLine();
 				
+				System.out.println("회원정보를 입력할까요(0/1)?");
+				int m_ = sc.nextInt();
+				sc.nextLine();
 				
-				
-				
+				if (m_ == 1) {
+					
+					//이름, 전화번호, 이메일 항목 저장용 1차원 배열
+					String[] member = new String[3];
+					member[0] = name;
+					member[1] = phone;
+					member[2] = email;
+					
+					//2차원 배열에 1차원 배열 객체를 저장
+					//2차원 배열의 인덱스 범위는 0 ~ (배열의 크기 - 1)
+					members[cnt] = member;
+					++cnt;
+					
+					System.out.println("회원 정보가 등록되었습니다.");
+				} else {
+					System.out.println("회원 정보 등록이 취소되었습니다.");
+				}
 				
 			} 
 		}
