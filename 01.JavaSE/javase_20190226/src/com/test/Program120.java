c7259112761807abec9349cf8158ae6b724d5a42
min0425 <min0425@naver.com>
2019-02-26 11:21:19 +0900

    외부입력-성적처리

--- 01.JavaSE/javase_20190226/src/com/test/Program120.java
+++ 01.JavaSE/javase_20190226/src/com/test/Program120.java
@@ -77,16 +77,33 @@ public class Program120 {
 			if (m == 0) break;
 			if (m == 1) {
 				System.out.println("1.성적정보출력");
-				
+
+				//배열의 성적 정보가 저장된 실제 인원수
+				int len = cnt;
+
 				//전체 성적에 대한 총점 계산 과정 추가
+				for (int a = 0; a < len; ++a) {
+					int[] score = scores[a];
+					score[4] = score[1] + score[2] + score[3];
+				}
 				
 				//전체 성적에 대한 등수 계산 과정 추가
 				//주의) 인원이 추가될 때마다 재계산 필요
+				//등수 계산용 초기화
+				for (int a = 0; a < len; ++a) {
+					int[] scoreA = scores[a];
+					scoreA[5] = 1;
+					for (int b = 0; b < len; ++b) {
+						int[] scoreB = scores[b];
+						if (scoreA[4] < scoreB[4]) {
+							++scoreA[5];
+						}
+					}
+				}			
 				
 				//전체 성적 정보 출력 과정 추가
-				int len = cnt;
 				System.out.println("--------------------------------");
-				System.out.println("학번/과목1/과목2/과목3/총점/평균");
+				System.out.println("학번/과목1/과목2/과목3/총점/등수");
 				for (int a = 0; a < len; ++a) {
 					int[] score = scores[a];
 					System.out.printf("%d / %d / %d / %d / %d / %d%n"
