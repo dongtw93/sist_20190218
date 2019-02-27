f504c21eb5872d1bb586b99f7f5dbd8a1a41d5ad
min0425 <min0425@naver.com>
2019-02-27 14:24:11 +0900

    BoardMarker 클래스 선언 및 실행

---
+++ 01.JavaSE/javase_20190227/src/com/test003/BoardMarker.java
@@ -0,0 +1,12 @@
+package com.test003;
+
+//실생활에 존재하는 대상을 객체 지향적 표현
+//->클래스
+//->예를 들어, 보드마카를 표현한다면 class BoardMarker { }
+public class BoardMarker {
+
+	//state -> 프로퍼티
+	//behavior -> 메소드
+
+	
+}
---
+++ 01.JavaSE/javase_20190227/src/com/test003/Main.java
@@ -0,0 +1,16 @@
+package com.test003;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//BoardMarker 클래스에 대한 인스턴스 생성
+		
+		//인스턴스 확인
+		
+		//메소드 호출
+		
+
+	}
+
+}
