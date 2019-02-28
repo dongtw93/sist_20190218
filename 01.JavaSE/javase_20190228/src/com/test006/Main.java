88b4af9c0f132b3169f55add31f5759c7299e84e
min0425 <min0425@naver.com>
2019-02-28 09:25:56 +0900

    20190228

---
+++ 01.JavaSE/javase_20190228/src/com/test006/Main.java
@@ -0,0 +1,16 @@
+package com.test006;
+
+
+//콘솔 UI 클래스
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//콘솔 관련 액션 코드 작성
+		//콘솔 입력(Scanner), 출력(System.out)
+		
+		
+
+	}
+
+}
