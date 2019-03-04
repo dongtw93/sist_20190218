cd95eb26d90792b2d80ab4bb315b779462156179
min0425 <min0425@naver.com>
2019-03-04 11:58:14 +0900

    진법 변환 메소드

---
+++ 01.JavaSE/javase_20190304/src/com/test017/Main.java
@@ -0,0 +1,17 @@
+package com.test017;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//문제) 십진수 숫자를 전달하면, 2진수 문자열을 반환하는 메소드
+		//출력예)
+		//1 -> 1
+		//2 -> 10
+		//3 -> 11
+		//...
+		//10 -> 1010
+
+	}
+
+}
---
+++ 01.JavaSE/javase_20190304/src/com/test017/Sample.java
@@ -0,0 +1,16 @@
+package com.test017;
+
+public class Sample {
+	
+	//매개 변수와 반환값이 모두 있는 경우
+	//매개 변수로는 정수 자료 1개, 반환값은 문자열 자료 1개
+	public String bin(int dec) {
+		
+		//진법 변환 액션 코드
+		//2로 나눈 나머지를 문자열 처리
+		//2로 나눈 몫이 0이 될 때까지 반복
+		
+		return "2진수 문자열";
+	}
+	
+}
