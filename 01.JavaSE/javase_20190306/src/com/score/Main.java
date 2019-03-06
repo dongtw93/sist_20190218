70d244025f76d88d456fe7453a26d5c7050abb5d
min0425 <min0425@naver.com>
2019-03-06 15:44:16 +0900

    성적관리 v1.0 (콘솔 버전)

---
+++ 01.JavaSE/javase_20190306/src/com/score/Main.java
@@ -0,0 +1,25 @@
+package com.score;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		//주메뉴 액션
+		//외부 입력 처리 준비 -> Scanner
+		
+		ScoreService s = new ScoreService();
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+		
+		sc.close();
+		System.out.println("프로그램 종료");
+		
+
+	}
+
+}
---
+++ 01.JavaSE/javase_20190306/src/com/score/ScoreService.java
@@ -0,0 +1,28 @@
+package com.score;
+
+public class ScoreService {
+
+	//HAS-A 관계 (강한 결합)
+	private ScoreDAO dao;
+	public ScoreService() {
+		this.dao = new ScoreDAO();
+	}
+	
+	//주메뉴당 메뉴별 액션
+	
+
+	//1.성적입력 메소드
+	
+	//2.성적출력 메소드
+	
+	//3.성적검색 메소드
+	//->서브메뉴
+	
+	//3.성적검색/1.번호기준 메소드
+	
+	//3.성적검색/1.이름기준 메소드
+	
+	//출력 포맷 private 메소드
+	
+	
+}
