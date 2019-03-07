0ed1ddcb562d9f4df3f239a028c80c06676c3c8a
min0425 <min0425@naver.com>
2019-03-07 09:45:58 +0900

    접근제한자 - private

--- 01.JavaSE/javase_20190307/src/com/test030/Main.java
+++ 01.JavaSE/javase_20190307/src/com/test030/Main.java
@@ -3,8 +3,18 @@ package com.test030;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		//private 접근제한자 테스트
+		
+		//객체(instance) 생성 + 생성자 호출(객체 초기화)
+		Sample s = new Sample();
+		
+		//외부에서 private 멤버 접근 시도
+		//System.out.println(s.a);
+		
+		//외부에서 public 멤버 접근 시도
+		s.method(); //10
+		
 	}
 
 }
