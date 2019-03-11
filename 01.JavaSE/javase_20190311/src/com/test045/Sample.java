f6bc8d33095469f8883edac1d2099882ab82f24c
min0425 <min0425@naver.com>
2019-03-11 10:13:31 +0900

    Object 클래스의 멤버(toString)에 대한 오버라이딩 상태 확인

---
+++ 01.JavaSE/javase_20190311/src/com/test045/Sample.java
@@ -0,0 +1,14 @@
+package com.test045;
+
+//부모 역할 클래스
+//Object 클래스를 자동 상속 받고 있다
+public class Sample {
+
+	//Object 클래스의 멤버(toString)에 대한 오버라이딩
+	//부모 메소드 시그니처는 그대로 유지
+	@Override
+	public String toString() {
+		return String.format("부모 역할 클래스 toString() 오버라이딩 메소드!");
+	}
+
+}
