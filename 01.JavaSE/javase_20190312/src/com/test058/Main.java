fc6975eb23327efa77015a316f47b1b6a0f41ec0
min0425 <min0425@naver.com>
2019-03-12 12:27:24 +0900

    인터페이스를 이용한 약한 결합

--- 01.JavaSE/javase_20190312/src/com/test058/Main.java
+++ 01.JavaSE/javase_20190312/src/com/test058/Main.java
@@ -8,13 +8,19 @@ public class Main {
 		
 		//Sample1 객체 준비
 		Sample1 s1 = new Sample1();
+		Sample2 s2 = new Sample2();
 		
 		//Sample3 객체에 Sample1 객체 공급
 		//Sample1 객체만 공급 가능
 		//->강한 결합
-		Sample3 s = new Sample3(s1);
+		Sample3 s3 = new Sample3(s1);
+		s3.method();
 		
-		s.method();
+		//Sample4 객체에 Sample1 객체 공급
+		//Sample1 객체 또는 Sample2 객체 공급 가능
+		//->인터페이스를 이용한 약한 결합
+		Sample4 s4 = new Sample4(s2);
+		s4.method();
 		
 	}
 
