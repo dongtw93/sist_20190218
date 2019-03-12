f640fcd3ab7294c46b18bf576ea3b88b8b020302
min0425 <min0425@naver.com>
2019-03-12 14:52:23 +0900

    직원관리 v1.0 콘솔 버전

---
+++ 01.JavaSE/javase_20190312/src/com/emp/domain/Region.java
@@ -0,0 +1,24 @@
+package com.emp.domain;
+
+//자료형 클래스 - 지역정보
+//출력시 지역번호 기준 정렬되도록 지원 -> Comparable 인터페이스 구현 필요
+public class Region implements Comparable<Region> {
+
+	//지역번호(REG01, REG02, ...), 지역명
+	
+	//생성자
+	
+	//getter, setter
+	
+	//toString() 오버라이딩
+	
+	
+	//compareTo() 오버라이딩
+	@Override
+	public int compareTo(Region o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+}
