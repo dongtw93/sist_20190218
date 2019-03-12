4c72f0da7e6ccea99ba251ff92dcee3b5277be67
min0425 <min0425@naver.com>
2019-03-12 09:50:42 +0900

    추상화 - 인터페이스

--- 01.JavaSE/javase_20190312/src/com/test054/Main.java
+++ 01.JavaSE/javase_20190312/src/com/test054/Main.java
@@ -3,7 +3,10 @@ package com.test054;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//추상 멤버에 대한 오버라이딩 확인
+		Sample s = new Sample();
+		s.method();
 
 	}
 
