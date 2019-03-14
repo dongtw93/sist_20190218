8434dea448b12c7fcbe9f7ee74481e159d67573e
min0425 <min0425@naver.com>
2019-03-14 11:07:41 +0900

    로또 판매 서비스

--- 01.JavaSE/javase_20190314/src/com/test065/Main.java
+++ 01.JavaSE/javase_20190314/src/com/test065/Main.java
@@ -1,11 +1,18 @@
 package com.test065;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		//메인 메뉴
 		//1.자동번호 2.수동번호 3.당첨자검색
 		
+		Scanner sc = new Scanner(System.in);
+		
+		LottoService service = new LottoService();
+		
+		service.menu1(sc);
 
 	}
 
