51f505f6d3c5608473bbc0ad805c78fb876bb0ab
min0425 <min0425@naver.com>
2019-03-14 09:04:02 +0900

    20190314

---
+++ 01.JavaSE/javase_20190314/src/com/test063/Main.java
@@ -0,0 +1,19 @@
+package com.test063;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		//컬렉션 프레임워크
+		//다수의 자료를 저장, 운영하는 클래스 집합
+		//List, Set, Map 인터페이스를 구현한 클래스
+		//List - 값(value)만 저장, 인덱스 제공, 저장 순서 유지, 중복 허용, 정렬 가능. 대표적으로 ArrayList 클래스
+		//Set - 값(value)만 저장, 인덱스 제공 불가, 저장 순서 유지 불가, 중복 불가, 정렬 불가. 대표적으로 HashSet 클래스
+		//Map - key, value 쌍으로 저장, key가 인덱스 역할, key는 중복 불가, value는 중복 허용. 대표적으로 HashMap 클래스
+
+		//Set 인터페이스 주요 메소드
+		
+
+	}
+
+}
