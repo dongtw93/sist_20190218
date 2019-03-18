437697776519ac0f2c54701c8997192695cbdb50
min0425 <min0425@naver.com>
2019-03-18 11:57:00 +0900

    SELECT

--- 02.Oracle/1일차_SELECT.sql
+++ 02.Oracle/1일차_SELECT.sql
@@ -23,6 +23,12 @@ DCL - Data Control Language
 GRANT
 REVOKE
 
+*/
+
+
+/*
+특정 테이블(자료에 물리적 저장소)에 대한 조회 명령
+주의)적절한 권한(CONNECT, RESOURCE role + SELECT ANY TABLE)이 부여된 경우만 조회 가능
 
 SELECT [컬럼명] 
     FROM [테이블명];
@@ -30,6 +36,13 @@ SELECT [컬럼명]
 특정 테이블의 자료 조회
 컬럼명 나열시 *(와일드카드) 사용 가능
 
+SELECT column1, column2....columnN
+    FROM   table_name
+    WHERE  condition
+    GROUP BY column_name
+    HAVING (arithematic function condition)
+    ORDER BY column_name {ASC|DESC};
+    
 */
 
 
