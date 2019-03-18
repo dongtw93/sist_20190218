1516e3a5255612f82b9dd70c17d56a0a9bcc6952
min0425 <min0425@naver.com>
2019-03-18 11:49:56 +0900

    SELECT

--- 02.Oracle/1일차_SELECT.sql
+++ 02.Oracle/1일차_SELECT.sql
@@ -1,4 +1,11 @@
 /*
+
+DB(Database) : 물리적 저장소에 저장된 자료 집합
+
+DBMS(Database Management System) : DB 운영 전용 소프트웨어. 대표적으로 Oracle, MySQL, ...
+
+SQL : DBMS에서 사용하는 (표준)명령어 집합
+
 SQL is a language to operate databases; it includes database creation, deletion, fetching rows, modifying rows, etc. 
 
 DDL - Data Definition Language
@@ -17,7 +24,8 @@ GRANT
 REVOKE
 
 
-SELECT [컬럼명] FROM [테이블명]
+SELECT [컬럼명] 
+    FROM [테이블명];
 
 특정 테이블의 자료 조회
 컬럼명 나열시 *(와일드카드) 사용 가능
@@ -28,4 +36,8 @@ SELECT [컬럼명] FROM [테이블명]
 --hr 계정의 employees 테이블에 대한 SELECT 쿼리 실행
 SELECT * 
     FROM hr.employees;
+    
+--hr 계정의 departments 테이블에 대한 SELECT 쿼리 실행
+SELECT *
+    FROM hr.departments;
     
